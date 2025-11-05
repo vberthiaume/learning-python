75c5e447fa94130df0b85aab28d72a78e45cc1cb
Vincent Berthiaume <vberthiaume@gmail.com>
2025-11-05 13:41:45 -0500

    ch2

--- Start/Ch2/vb_summary.py
+++ Start/Ch2/vb_summary.py
@@ -30,3 +30,40 @@ else:
 
 # conditional statements on one line (elif not supported here)
 print ("x is less than y" if x < y else "x is the same or bigger than y bro")
+
+# define a while loop
+# answer = input("should I stay or should I go? ")
+# while answer != "go":
+#     print (answer)
+#     answer = input("should I stay or should I go? ")
+
+# define a for loop
+days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+for d in days:
+    if d == "thu":
+        break
+    if d == "tue":
+        continue
+    print (d)
+
+# using the enumerate() function to get an index and an item
+for i, d in enumerate(days): #i, d here is a tuple
+    print (i,d)
+
+# regular function
+def cube(x, greeting=None):
+    print(greeting)
+    return x * x * x
+
+print (cube(2))                         # uses the default value for greeting
+print (cube(2, "brooo"))                # set the greeting normally
+print (cube(greeting = "before", x=2))  # set the greeting out of order explicitly
+
+# function with variable number of parameters
+def multi_add(*args):   # the * here means an indefinite number of arguments. Doesn't need to be named args
+    result = 0
+    for x in args:
+        result += x
+    return result
+
+print (multi_add(1, 2, 3, 4, 5))
